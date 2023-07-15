e79d9f4ea0dc09789883013d3d45f7b9acb4c691
Badx99 <badrallpro@gmail.com>
2023-07-14 19:54:09 -0700

    if and loops

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+/**
+ * main - main
+ * Description: Print all possible two-digit combinations
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+	int y = 0;
+
+	while (y < 10)
+	{
+		x = 0;
+		while (x < 10)
+		{
+			if (y != x && y < x)
+			{
+				putchar('0' + y);
+				putchar('0' + x);
+
+				if (x + y != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+			x++;
+		}
+		y++;
+	}
+	putchar('\n');
+	return (0);
+}
