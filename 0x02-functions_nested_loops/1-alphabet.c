768980c1a03454e489f560ede7a2eceeaea5a029
Badx99 <badrallpro@gmail.com>
2023-07-18 19:40:54 -0700

    commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,23 +1,20 @@
 #include "main.h"
 
 /**
- * print_alphabet - ptints alphabet 
- *
- * Return: void
+ * print_alphabet - Entry point
+ * Return:0
  */
 
 void print_alphabet(void)
 {
-	char c;
-	c = 'a';
+	char c = 'a';
+
 	while (c <= 'z')
 	{
-		
-	        _putchar(c);
-	        c++;
+		_putchar(c);
+		c++;
+
 	}
 	_putchar('\n');
-		
-	
 
 }
