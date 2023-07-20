e2e3e03f511569ab3e3d90692fc83330fe243907
Badx99 <badrallpro@gmail.com>
2023-07-20 14:29:50 -0700

    commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,33 +1,42 @@
 #include <stdio.h>
-
 /**
- * main - fizz buzz
- * Return:0
+ * main - check the code for Holberton School students.
+ *
+ * Return: void.
  */
 
 int main(void)
 {
+
 	int i = 1;
+
 	while (i <= 100)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz");
 		}
-	 	else if (i % 3 == 0)
+		else if (i % 3 == 0)
 		{
-			printf ("Fizz");
-	        }
+			printf("Fizz");
+		}
 		else if (i % 5 == 0)
 		{
-			printf ("Buzz");
-	        }
+			printf("Buzz");
+		}
 		else
 		{
-			printf ("%d",i);
+			printf("%i", i);
 		}
-			
-			
+
+		if (i != 100)
+		{
+			putchar(' ');
+		}
+
 		i++;
 	}
+	putchar('\n');
+	return (0);
+
 }
