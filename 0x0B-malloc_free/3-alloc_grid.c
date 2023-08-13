d33938dda32fdd7794b9fad03c9532cf16fee472
Badx99 <badrallpro@gmail.com>
2023-08-12 20:12:09 -0700

    commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,12 +1,13 @@
-include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
+
 /**
  * alloc_grid - prints a grid of integers
  * @width: width of the grid
  * @height: height of the grid
- *
  * Return: pointer..
  */
+
 int **alloc_grid(int width, int height)
 {
 int **s, r, c;
